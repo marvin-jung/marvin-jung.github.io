0f540e6da214b657f6c130f05efa7281af6efe24
marvin-jung <68761578+marvin-jung@users.noreply.github.com>
2021-10-25 21:41:14 +0900

    change name in display

--- src/components/About.js
+++ src/components/About.js
@@ -18,7 +18,7 @@ export default function About(props) {
         <section id="about">
             <div className="row">
                 <div className="three columns">
-                    <img className="profile-pic" src={profilepic} alt="Hermione Granger Profile Picture" />
+                    <img className="profile-pic" src={profilepic} alt="Jisung Jung Profile Picture" />
                 </div>
                 <div className="nine columns main-col">
                     <h2>About Me</h2>
